84f212cda716958327a340f5144e5a1f0e7732a5
kphsb <k.homsombath@nuol.edu.la>
2018-08-08 14:26:44 +0700

    Update Upload Photo and Email to Firebase

--- app/src/main/java/la/edu/homsombathnuol/k/laosunseen/fragment/RegisterFragment.java
+++ app/src/main/java/la/edu/homsombathnuol/k/laosunseen/fragment/RegisterFragment.java
@@ -6,6 +6,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
@@ -15,8 +16,19 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.Collections;
 
@@ -29,6 +41,9 @@ public class RegisterFragment extends Fragment {
     private Uri uri;
     private ImageView imageView;
     private boolean aBoolean = true;
+    private String nameString;
+    private String emailString;
+    private String passwordString;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -58,25 +73,88 @@ public class RegisterFragment extends Fragment {
     }
 
     private void uploadProcess() {
+        EditText nameEditText = getView().findViewById(R.id.edtName);
+        EditText emailEditText = getView().findViewById(R.id.edtEmail);
+        EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+//        Get Value From Editext
+        nameString = nameEditText.getText().toString().trim();
+         emailString = emailEditText.getText().toString().trim();
+         passwordString = passwordEditText.getText().toString().trim();
+
 //        Check Choose Photo
         if(aBoolean){
 //            Non Choose Photo
             MyAlert myAlert = new MyAlert(getActivity());
-            myAlert.nomalDialog("Non Choose Photo","Please Chosse Photo");
+            myAlert.nomalDialog("Non Choose Photo",
+                    "Please Chosse Photo");
+        } else if (nameString.isEmpty() || emailString.isEmpty() || passwordString.isEmpty()) {
+//            Have Space
+            MyAlert myAlert = new MyAlert(getActivity());
+            myAlert.nomalDialog("Have Space",
+                    "Please Fill All Every Blank");
+
+        } else {
+//No Space
+            createAuthentication();
+            uploadPhotoToFirebase();
         }
+
     }
+
+    private void createAuthentication() {
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.createUserWithEmailAndPassword(emailString, passwordString)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            Toast.makeText(getActivity(),"Register Successful",Toast.LENGTH_SHORT);
+                        }else {
+                            MyAlert myAlert = new MyAlert(getActivity());
+                            myAlert.nomalDialog("Cannot Register",
+                                    "Because ==>" +task.getException().getMessage());
+                        }
+
+                    }
+                });
+
+
+    }
+
+    private void uploadPhotoToFirebase() {
+
+        FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
+        StorageReference storageReference = firebaseStorage.getReference();
+        StorageReference storageReference1 = storageReference.child("Avata/" + nameString);
+
+        storageReference1.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(getActivity(),"Success Upload Photo",Toast.LENGTH_SHORT);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getActivity(),"Cannot Upload Photo",Toast.LENGTH_SHORT);
+            }
+        });
+
+    }//UploadPhoto
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == getActivity().RESULT_OK) {
             uri = data.getData();
+            aBoolean = false;
             try{
                 Bitmap bitmap = BitmapFactory
                         .decodeStream(getActivity()
                                 .getContentResolver()
                                 .openInputStream(uri));
-                Bitmap bitmap1 = Bitmap.createScaledBitmap(bitmap,400,300,false);
+                Bitmap bitmap1 = Bitmap.createScaledBitmap(bitmap,500,300,false);
                 imageView.setImageBitmap(bitmap1);
 
             }catch (Exception e){
