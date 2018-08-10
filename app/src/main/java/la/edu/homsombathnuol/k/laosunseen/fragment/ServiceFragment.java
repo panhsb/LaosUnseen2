7b653f7ee91041c8381926671d09cec6030e9610
kphsb <k.homsombath@nuol.edu.la>
2018-08-10 10:02:19 +0700

    Start 10Aug Post

--- app/src/main/java/la/edu/homsombathnuol/k/laosunseen/fragment/ServiceFragment.java
+++ app/src/main/java/la/edu/homsombathnuol/k/laosunseen/fragment/ServiceFragment.java
@@ -2,30 +2,90 @@ package la.edu.homsombathnuol.k.laosunseen.fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
 
 import la.edu.homsombathnuol.k.laosunseen.MainActivity;
 import la.edu.homsombathnuol.k.laosunseen.R;
 
 public class ServiceFragment extends Fragment {
 
+    private String nameString, currentPostString, uidString;
+    private String tag = "10AugV1";
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        findMyMe();
+
+//        Post Controller
+        postController();
 
     }//Main Method
+
+    private void findMyMe() {
+
+        FirebaseAuth firebaseAuth  = FirebaseAuth.getInstance();
+        uidString = firebaseAuth.getCurrentUser().getUid();
+
+        Log.d(tag,"Uid ==> " + uidString);
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference().child("User").child(uidString);
+
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                Map map = (Map) dataSnapshot.getValue();
+                nameString = String.valueOf(map.get("nameString"));
+                currentPostString = String.valueOf(map.get("myPostString"));
+                Log.d(tag,"Name ==> " + nameString);
+                Log.d(tag,"Current Post ==> " + currentPostString);
+                Toast.makeText(getActivity(),"Name ==> " + nameString, Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    private void postController() {
+        Button button  = getView().findViewById(R.id.btnPost);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
 
     @Nullable
     @Override
